4258cd40ff908d97e6a1cf275bc053c2188d507d
Xavier Medrano <xmedr10@gmail.com>
2021-03-24 13:01:18 -0400

    Started setting up variables for the app.js file

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+this project is being created using Cathy Dutton's code 
+(https://codepen.io/cathydutton/pen/GBcvo) as a guide to learn
+okay so what it seems like i need to be doing is getting the seconds and miliseconds
+assigning them to a variable, and then updating the page to show that variable.
+so i should use getElementById to assign each element to a variable
+*/
+
+var seconds = 00;
+var tens = 00;
+var appendSeconds = document.getElementById('seconds');
+var appendTens = document.getElementById('tens');
+var buttonStart = document.getElementById('button-start');
+var buttonStop = document.getElementById('button-stop');
+var buttonReset = document.getElementById('button-reset');
+
